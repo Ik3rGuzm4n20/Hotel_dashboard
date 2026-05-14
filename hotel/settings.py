0ac43baf1029425464ffcdb214348799b7baa776
Iker Guzman <iaguzman2004hp@gmail.com>
2026-05-13 21:07:59 -0600

    Configuracion de base de datos MySQL

--- hotel/settings.py
+++ hotel/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,7 +76,11 @@ WSGI_APPLICATION = 'hotel.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'hotel_db',
+        'USER': 'root',
+        'PASSWORD': 'Ikerguzman2004$',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
